be8d9b0aef383968f28702bcec051a9b5d648892
Kingcedru <byishimocedrick@gmail.com>
2023-10-12 15:58:24 +0200

    nested routes

--- routerExercises/src/App.jsx
+++ routerExercises/src/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./Home"
-import About from "./about"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Van"
 
-function App() {
+import "./server"
+import VanDetail from './pages/VanDetail';
+
+export default function App() {
   return (
     <BrowserRouter>
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
-      
     </BrowserRouter>
   )
 }
 
-export default App
--- routerExercises/src/pages/about.jsx
+++ routerExercises/src/pages/about.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import bgImg from "./assets/backi1.png"
+import bgImg from "../assets/backi1.png"
 import { Link } from "react-router-dom"
 
 export default function About() {
